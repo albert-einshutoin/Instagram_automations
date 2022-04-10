dbfd7902622a3d54c2f1eb0bc1241b9a33e7ef2f
Shuto Ide <shuto@ShutonoMacBook-puro.local>
2022-04-10 09:05:53 +0900

    [add] visualise dir

---
+++ visualise/main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+class Visualise(object):
+
+    def __init__(self, name):
+        self.name = name
+
+    def get_hashtags(self):
+        df = pd.read_csv('')
+        return hashtags
