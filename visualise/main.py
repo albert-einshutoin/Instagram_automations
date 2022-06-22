ae68af8121112bb5ed05e2eb084e8f8203004aa0
Shuto <einstein.4s.1110@gmail.com>
2022-06-22 12:50:07 +0900

    [add] like_variation func

--- visualise/main.py
+++ visualise/main.py
@@ -1,13 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import mysql.connector
+import numpy as np
+from statistics import mean
+
+import settings as st
 
 
 class Visualise(object):
 
     def __init__(self, target):
         self.target = target
+        self.user_id = self.get_user_id()
 
-    def get_hashtags(self):
+    def get_user_id(self):
         """
         """
         cnx = mysql.connector.connect(user=st.DB_USER,
@@ -15,9 +21,62 @@ class Visualise(object):
                                       host='127.0.0.1',
                                       database=st.DB_NAME)
         cursor = cnx.cursor()
-        cursor.execute('SELECT * from tags where post_id=')
+        cursor.execute(f'SELECT id from user where username="{self.target}"')
+        result = cursor.fetchone()
+        cursor.close()
+        cnx.close()
+        return result[0]
 
-        cnx.commit()
+    def post_frecuency(self):
+        cnx = mysql.connector.connect(user=st.DB_USER,
+                                      password=st.DB_PASS,
+                                      host='127.0.0.1',
+                                      database=st.DB_NAME)
+        cursor = cnx.cursor()
+        # get posted_time from latest date
+        cursor.execute(f'SELECT posted_time from post_info where\
+                user_id={self.user_id} order by posted_time desc')
+        result = cursor.fetchall()
+        posted_time = [result[i][0] for i in range(len(result))]
+        frecuency = [posted_time[i-1] - posted_time[i] for i in range(len(posted_time))]
+        cursor.close()
+        cnx.close()
+        print(frecuency)
+
+    def get_post_id(self):
+        """
+        """
+        cnx = mysql.connector.connect(user=st.DB_USER,
+                                      password=st.DB_PASS,
+                                      host='127.0.0.1',
+                                      database=st.DB_NAME)
+        cursor = cnx.cursor()
+        cursor.execute('SELECT post_id from post_info where user_id="{self.user_id}"')
+        result = cursor.fetchall()
+        cursor.close()
+        cnx.close()
+        return result
+
+    def get_liked_count(self):
+        """
+        """
+        cnx = mysql.connector.connect(user=st.DB_USER,
+                                      password=st.DB_PASS,
+                                      host='127.0.0.1',
+                                      database=st.DB_NAME)
+        cursor = cnx.cursor()
+        cursor.execute('SELECT post_id from post_info where user_id="{self.user_id}"')
+
+    def get_hashtags(self, post_id=None):
+        """
+        """
+        cnx = mysql.connector.connect(user=st.DB_USER,
+                                      password=st.DB_PASS,
+                                      host='127.0.0.1',
+                                      database=st.DB_NAME)
+        cursor = cnx.cursor()
+        cursor.execute(f'SELECT * from tags where post_id="{post_id}"')
+
         cursor.close()
         cnx.close()
         return hashtags
@@ -32,3 +91,9 @@ class Visualise(object):
 
         plt.show()
 # def hashtags_similality(self):
+
+
+vis = Visualise(target="sharedanshi")
+# vis.get_hashtags()
+vis.post_frecuency()
+
